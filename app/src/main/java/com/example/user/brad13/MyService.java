43aac265c5681225c10c55f5970836c54d55e7ef
Brad Chao <brad@brad.tw>
2016-11-24 10:28:13 +0800

    v2

--- app/src/main/java/com/example/user/brad13/MyService.java
+++ app/src/main/java/com/example/user/brad13/MyService.java
@@ -5,7 +5,13 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyService extends Service {
+    private Timer timer;
+    private int i;
+
     public MyService() {
     }
 
@@ -19,18 +25,35 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.v("brad", "onCreate()");
+        timer = new Timer();
+        timer.schedule(new MyTask(), 1000, 1000);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.v("brad", "onStartCommand()");
+        boolean isStart = intent.getBooleanExtra("isstart",false);
+        if (isStart){
+            i = intent.getIntExtra("value",-100);
+        }
+
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.v("brad", "onDestroy()");
+        if (timer != null){
+            timer.purge();
+            timer.cancel();
+            timer = null;
+        }
     }
+
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            Log.v("brad", "i = " + i++);
+        }
+    }
+
 }
