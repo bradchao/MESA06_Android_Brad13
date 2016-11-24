f7f2c6600d7495e48510749b697b986c043125c2
Brad Chao <brad@brad.tw>
2016-11-24 10:03:25 +0800

    v1

---
+++ app/src/main/java/com/example/user/brad13/MyService.java
@@ -0,0 +1,36 @@
+package com.example.user.brad13;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v("brad", "onCreate()");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.v("brad", "onStartCommand()");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("brad", "onDestroy()");
+    }
+}
